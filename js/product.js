64dc575b9dd395a60d0cfc008343c7582ba0ec69
minhluquang <luminh2004@gmail.com>
2024-02-27 22:09:02 +0700

    Reponsive giao diện Product Page, Cài đặt font Mulish

--- js/product.js
+++ js/product.js
@@ -1,4 +1,4 @@
-const btnDetail = document.querySelector(".product-action__detail");
+const btnDetail = document.querySelectorAll(".product-action__detail");
 const btnCloseModal = document.querySelector(".modal-close-icon");
 const btnDecreaseQnt = document.querySelector(".modal-qnt__descrease");
 const btnIncreaseQnt = document.querySelector(".modal-qnt__increase");
@@ -6,9 +6,11 @@ const btnIncreaseQnt = document.querySelector(".modal-qnt__increase");
 const overlay = document.querySelector(".overlay");
 const modal = document.querySelector(".modal");
 
-btnDetail.addEventListener("click", (e) => {
-  modal.classList.add("active");
-  overlay.classList.add("active");
+btnDetail.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    modal.classList.add("active");
+    overlay.classList.add("active");
+  });
 });
 
 btnCloseModal.addEventListener("click", (e) => {
@@ -36,3 +38,14 @@ btnIncreaseQnt.addEventListener("click", (e) => {
   modalQntValue.value = qnt + 1;
   modalQntValue.setAttribute("value", qnt + 1);
 });
+
+// Start: Handle Sort When Small Devices (Select, option)
+const sortFilterSelect = document.querySelector(".sort-filter__select");
+
+sortFilterSelect.addEventListener("change", (e) => {
+  const link = `sortby=${e.target.value}`;
+  const host = window.location.origin;
+  const pathname = window.location.pathname;
+  window.location.href = `${host}${pathname}?${link}`;
+});
+// End: Handle Sort When Small Devices (Select, option)
