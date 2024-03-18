2d6262d69339ae472195aee3bae7fd90832b4c46
mhtri28 <minhtrii2810@gmail.com>
2024-03-18 19:07:47 +0700

    adminProduct

--- index.php
+++ index.php
@@ -1,38 +1,37 @@
 <?php
 
-  // Import connectDB
-  include_once('model/connect.php');
+// Import connectDB
+include_once('model/connect.php');
 
-  // Header
-  require_once("view/header.php");
+// Header
+require_once("view/header.php");
 
-  // Content
-  if (isset($_GET['page']) && $_GET['page'] != '') {
-    $page = $_GET['page'];
+// Content
+if (isset($_GET['page']) && $_GET['page'] != '') {
+  $page = $_GET['page'];
 
-    switch ($page) {
-      case 'product':
-        require_once('view/pages/product.php');
-        break;
-      case 'cart':
-        require_once('view/pages/cart.php');
-        break;
-      case 'checkout':
-        require_once('view/pages/checkout.php');
-        break;
-      case 'signup':
-        require_once('view/pages/signup.php');
-        break;
-      default:
-        // Xử lý trường hợp không khớp với bất kỳ trang nào
-        require_once('view/pages/notFound.php');
-        break;
-    }
-  } else {
-    // Trang chủ
-    require_once('view/pages/home.php');
+  switch ($page) {
+    case 'product':
+      require_once('view/pages/product.php');
+      break;
+    case 'cart':
+      require_once('view/pages/cart.php');
+      break;
+    case 'checkout':
+      require_once('view/pages/checkout.php');
+      break;
+    case 'signup':
+      require_once('view/pages/signup.php');
+      break;
+    default:
+      // Xử lý trường hợp không khớp với bất kỳ trang nào
+      require_once('view/pages/notFound.php');
+      break;
   }
+} else {
+  // Trang chủ
+  require_once('view/pages/home.php');
+}
 
-  // Footer
-  require_once("view/footer.php");
-?>
+// Footer
+require_once("view/footer.php");
