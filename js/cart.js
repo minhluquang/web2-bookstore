436e34425f1abeebc729568c20c19a94662f08a0
mhtri28 <minhtrii2810@gmail.com>
2024-03-23 10:01:55 +0700

    fix

--- js/cart.js
+++ js/cart.js
@@ -4,12 +4,16 @@ const btnAddGroup = document.querySelectorAll(".btn-add-qty");
 btnSubstractGroup.forEach(btn =>
   btn.addEventListener("click", function (e) {
     const input = btn.nextElementSibling;
-    if (input.value > 0) --input.value;
+    if (input.value > 1) {
+      --input.value;
+      input.setAttribute("value", input.value);
+    }
   })
 );
 btnAddGroup.forEach(btn =>
   btn.addEventListener("click", function (e) {
     const input = btn.previousElementSibling;
     ++input.value;
+    input.setAttribute("value", input.value);
   })
 );
