bfea8f0d1ad633af6195c7f55a92b16746a9be18
triwjbu <minhtriqt04@gmail.com>
2024-03-31 12:31:08 +0700

    pagnation update

--- js/admin/order.js
+++ js/admin/order.js
@@ -3,39 +3,69 @@ var script = document.createElement("SCRIPT");
 script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName("head")[0].appendChild(script);
-
+var search = location.search.substring(1);
+urlParams = JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g, '":"') + '"}', function (key, value) { return key === "" ? value : decodeURIComponent(value) })
+var number_of_item = urlParams['item'];
+var current_page = urlParams['pag'];
+if (current_page == null) {
+    current_page = 1;
+}
+if (number_of_item == null) {
+    number_of_item = 5;
+}
 function checkReady() {
-  return new Promise(async function(resolve) {
-    while (!window.jQuery) {
-        await new Promise(resolve => setTimeout(resolve, 20));
-    }
-    resolve();
-  })
+    return new Promise(async function (resolve) {
+        while (!window.jQuery) {
+            await new Promise(resolve => setTimeout(resolve, 20));
+        }
+        resolve();
+    })
 }
-async function loadForFirstTime(){
-  await checkReady();
-    loadItem(1);
+async function loadForFirstTime() {
+    await checkReady();
+    loadItem();
 }
-const urlParams = new URLSearchParams(window.location.search);
-function loadItem (current_page) {
-    $.ajax({
-    url: '../controller/admin/pagnation.controller.php',
-    type: "post",
-    dataType: 'html',
-    data: {
-      number_of_item: 5,
-      current_page: current_page,
-      function: "render"
-    }
-  }).done(function (result) {
-    $('.result').html(result);
-    js();
-  })
-};
+function pagnationBtn() {
+    // pagnation
+    document.querySelectorAll('.pag').forEach((btn) => btn.addEventListener('click', function () {
+        current_page=btn.innerHTML;
+        loadItem();
+    }));
+    if (document.getElementsByClassName('pag-pre').length > 0)
+        document.querySelector('.pag-pre').addEventListener('click', function () {
+            current_page = Number(document.querySelector('span.active').innerHTML) - 1;
+            loadItem(number_of_item, current_page);
+        });
+    if (document.getElementsByClassName('pag-con').length > 0)
+        document.querySelector('.pag-con').addEventListener('click', function () {
+            current_page = Number(document.querySelector('span.active').innerHTML) + 1;
 
-document.addEventListener("DOMContentLoaded", () => { 
-  loadForFirstTime()
+            loadItem();
+        });
+}
+function loadItem() {
+    $.ajax({
+        url: '../controller/admin/pagnation.controller.php',
+        type: "post",
+        dataType: 'html',
+        data: {
+            number_of_item: number_of_item,
+            current_page: current_page,
+            function: "render"
+        }
+    }).done(function (result) {
+        var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?page=' + urlParams['page'] + '&item=' + number_of_item + '&pag=' + current_page ;
+        window.history.pushState({path:newurl},'',newurl);
+        $('.result').html(result);
+        pagnationBtn();
+        js();
+
+    })
+};
+document.addEventListener("DOMContentLoaded", () => {
+    loadForFirstTime()
 });
+
 var js = function () {
 
   const btnDetails = document.querySelectorAll(".actions--view");
