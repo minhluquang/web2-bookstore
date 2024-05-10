ec60399a20fc32db8b16352e17324ea24bef7e8c
triwjbu <minhtriqt04@gmail.com>
2024-05-11 04:59:56 +0700

    thống kê

--- admin/public/home.php
+++ admin/public/home.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="../css/fonts/fonts.css?v=<?php echo time(); ?>" />
     <link rel="stylesheet" href="../assets/fontawesome-free-6.5.1-web/css/all.css?v=<?php echo time(); ?>" />
     <link rel="stylesheet" href="../css/admin/home.css?v=<?php echo time(); ?>" />
+    <link rel="stylesheet" href="../css/admin/product.css?v=<?php echo time(); ?> " />
+    <link rel="stylesheet" href="../css/admin/filter.css?v=<?php echo time(); ?> " />
 </head>
 
 <body>
@@ -65,45 +67,21 @@
         <div class="content-container">
             <div class="date">
                 <h3>Từ ngày:</h3>
-                <input type="date" id="startdate" name="startdate">
+                <input type="date" id="startdate" name="startdate" value="<?php date_default_timezone_set('Asia/Ho_Chi_Minh');
+                                                                            echo date('Y-m-01', time()); ?>">
 
             </div>
             <div class="date">
                 <h3>Đến ngày:</h3>
-                <input type="date" id="enddate" name="enddate">
+                <input type="date" id="enddate" name="enddate" value="<?php date_default_timezone_set('Asia/Ho_Chi_Minh');
+                                                                        echo date('Y-m-d', time()); ?>">
 
             </div>
             <button type="button" id="filter">Lọc</button>
         </div>
         <div class="content-container" id="thongke-container">
-            <!-- <?php
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
 
-            // Create connection
-            $conn = mysqli_connect($servername, $username, $password, "backend_web2");
 
-            // Check connection
-            if (!$conn) {
-                die("Connection failed: " . mysqli_connect_error());
-            }
-
-            $sql = 'SELECT * FROM categories';
-            $result = mysqli_query($conn, $sql);
-
-            while ($row = mysqli_fetch_array($result)) {
-                echo '<div class="thongke">';
-                echo '<h3 class="sanpham">'.$row[1].'</h3>';
-                echo '                <div class="money"> 0 VND</div>
-                <div class="soluong">Số lượng bán được: 0</div>
-                <button type="button" class="chitietbtn">Chi tiết</button>
-                </div>';
-
-            }
-            $conn->close();
-            ?> -->
-            
         </div>
     </div>
     <div class="popup">
@@ -112,86 +90,16 @@
                 <div class="popupmenu" id="chitiet-table">
                     <div id="title">
                         <span>Thể loại</span>
-                        <button type="button" id="close" onclick="popupToggle('chitiet');"><i class="fa-solid fa-xmark"></i></button>
-                        <!-- <hr> -->
+                        <button type="button" id="close"><i class="fa-solid fa-xmark"></i></button>
+ 
                     </div>
                     <div class="table-container">
-
-                        <table class="thongke-chitiet">
-                            <thead>
-                                <tr>
-                                    <th>Mã SP</th>
-                                    <th>Ảnh</th>
-                                    <th>Tên SP</th>
-                                    <th>Giá gốc</th>
-                                    <th>Số lượng</th>
-                                </tr>
-                            </thead>
-                            <tr>
-                                <td>masp</td>
-                                <td class="image">
-                                    <img src="../assets/image/products/image_1.jpg" alt="" />
-                                </td>
-                                <td>Ten sp</td>
-                                <td>400000</td>
-                                <td>5</td>
-                            </tr>
-                            <tr>
-                                <td>masp</td>
-                                <td class="image">
-                                    <img src="../assets/image/products/image_1.jpg" alt="" />
-                                </td>
-                                <td>Ten sp</td>
-                                <td>400000</td>
-                                <td>5</td>
-                            </tr>
-                            <tr>
-                                <td>masp</td>
-                                <td class="image">
-                                    <img src="../assets/image/products/image_1.jpg" alt="" />
-                                </td>
-                                <td>Ten sp</td>
-                                <td>400000</td>
-                                <td>5</td>
-                            </tr>
-                            <tr>
-                                <td>masp</td>
-                                <td class="image">
-                                    <img src="../assets/image/products/image_1.jpg" alt="" />
-                                </td>
-                                <td>Ten sp</td>
-                                <td>400000</td>
-                                <td>5</td>
-                            </tr>
-                        </table>
-
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <script>
-        var btns = document.getElementsByClassName("chitietbtn");
-
-        var btnPopup = function() {
-            popupToggle('chitiet');
-            changeTitle(this);
-        };
-
-        for (var i = 0; i < btns.length; i++) {
-            btns[i].addEventListener('click', btnPopup);
-        }
-
-        function popupToggle(idname) {
-            var popup = document.getElementById(idname);
-            popup.classList.toggle("show");
-        }
-
-        function changeTitle(newTitle) {
-            document.getElementById("title").querySelector("span").innerHTML = newTitle.parentNode.querySelector(".sanpham").innerHTML;
-        }
-    </script>
 </body>
 
 </html>
