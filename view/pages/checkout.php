62b6923b26726007290597c793e0b04e0afbbb13
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-10-25 10:01:37 +0700

    fix 42,43,44,46,47,48

--- view/pages/checkout.php
+++ view/pages/checkout.php
@@ -83,9 +83,9 @@
 
                                 </div>
                                 <div style="display: flex;flex-direction:row-reverse;padding-top:5px">
-                                    <button type="button" class="confirm create_address hidden">Xác Nhận</button>
-                                    <button type="button" class="confirm change_address">Xác Nhận</button>
-                                    <button type="button" class="cancel change_address"> Hủy</button>
+                                    <button class="confirm create_address hidden">Xác Nhận</button>
+                                    <button class="confirm change_address">Xác Nhận</button>
+                                    <button class="cancel change_address"> Hủy</button>
 
                                 </div>
                             </div>
